ec561743a4ce1da88d0f9d715511d7f47b2c014d
mrampazz <mrampazz@gmail.com>
2020-02-27 19:18:52 +0100

    Aggiunta versione aggiornata del POC1

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders VRAM Software Web App - PoC 1 text', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/VRAM Software Web App - PoC 1/i);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
