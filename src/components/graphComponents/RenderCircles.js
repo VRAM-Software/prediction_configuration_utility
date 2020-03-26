43d13b44353f92de3ddfc41e22e965c05fb389e8
mrampazz <mrampazz@gmail.com>
2020-03-26 18:37:57 +0100

    Aggiunto label on hover dei dati

--- src/components/graphComponents/RenderCircles.js
+++ src/components/graphComponents/RenderCircles.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "../../assets/App.css";
 export default class RenderCircles extends React.Component {
     handleClick = (point) => {
         console.log(point.x, point.y);
@@ -7,23 +7,35 @@ export default class RenderCircles extends React.Component {
     render() {
         const renderCircles = this.props.data.map((item, index) => (
             parseInt(item.label) === 1 ?
-            <circle
-                onClick={() => this.handleClick({x: item.weight, y: item.size})}
-                cx={this.props.scale.x(item.weight)}
-                cy={this.props.scale.y(item.size)}
-                r="4"
-                style={{ fill: "green" }}
-                key={index}
-            />
+            <>
+                <circle
+                    className="tooltip"
+                    onClick={() => this.handleClick({x: item.weight, y: item.size})}
+                    cx={this.props.scale.x(item.weight)}
+                    cy={this.props.scale.y(item.size)}
+                    r="4"
+                    style={{ fill: "green" }}
+                    key={index}
+                />
+                <text className="tooltipLabel" x={20} y={0} fill="black">
+                    ({item.weight}, {item.size})
+                </text>
+            </>
             :
-            <circle
-                onClick={() => this.handleClick({x: item.weight, y: item.size})}
-                cx={this.props.scale.x(item.weight)}
-                cy={this.props.scale.y(item.size)}
-                r="4"
-                style={{ fill: "red" }}
-                key={index}
-            />
+            <>
+                <circle
+                    className="tooltip"
+                    onClick={() => this.handleClick({x: item.weight, y: item.size})}
+                    cx={this.props.scale.x(item.weight)}
+                    cy={this.props.scale.y(item.size)}
+                    r="4"
+                    style={{ fill: "red" }}
+                    key={index}
+                />
+                <text className="tooltipLabel" x={20} y={0} fill="black">
+                    ({item.weight}, {item.size})
+                </text>
+            </>
         ));
         return <g>{renderCircles}</g>;
     }
