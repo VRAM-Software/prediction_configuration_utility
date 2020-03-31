9c8d5329165aa794938209710ef8bced33273c2d
Vittorio Santagiuliana <vittoriosantagiuliana@gmail.com>
2020-03-31 18:20:38 +0200

    Risolti ultimi code smells

--- src/algorithm/regression.module.js
+++ src/algorithm/regression.module.js
@@ -76,9 +76,9 @@ class Regression {
         var hypothesis = []
         console.log("coefficients lenght")
         console.log(this.coefficients.length)
-        for(var x = 0; x < this.coefficients.length; x++) {
+        for(let x = 0; x < this.coefficients.length; x++) {
             var coefficientRow = this.coefficients[x]
-            for(var y = 0; y < coefficientRow.length; y++) {
+            for(let y = 0; y < coefficientRow.length; y++) {
                 console.log("option")
                 console.log(options.x[x])
                 hypothesis[y] = (hypothesis[y] || 0) + coefficientRow[y] * options.x[x]
@@ -91,11 +91,11 @@ class Regression {
     inverse(matrix, identity) {
         var size = matrix.length
         var result = new Array(size)
-        for(var i = 0; i < size; i++){
+        for(let i = 0; i < size; i++){
             result[i] = matrix[i].concat(identity[i])
         }
         result = this.rref(result)
-        for(var i = 0; i < size; i++) {
+        for(let i = 0; i < size; i++) {
             result[i].splice(0, size)
         }
         return result
@@ -143,10 +143,10 @@ class Regression {
     multiply(lhs, rhs) {
         var options ={ numRows: lhs.length, numColumns: rhs[0].length }
         var streamingProduct = this.rectMatrix(options)
-        for(var x = 0; x < rhs.length; x++) {
+        for(let x = 0; x < rhs.length; x++) {
             var lhsColumn = []
             // Get the xth column of lhs.
-            for(var r = 0; r < lhs.length; r++) {
+            for(let r = 0; r < lhs.length; r++) {
                 lhsColumn.push(lhs[r][x])
             }
             // Get the xth row of rhs.
@@ -162,7 +162,7 @@ class Regression {
     //Ritorna una matrice identità con tutte le celle a 1
     identityMatrix(size) {
         var matrix = this.rectMatrix({ numRows: size, numColumns: size })
-        for(var i = 0; i < size; i++) {
+        for(let i = 0; i < size; i++) {
             matrix[i][i] = 1
         }
         return matrix
@@ -171,10 +171,10 @@ class Regression {
     //Ritorna una matrice numRows x numColumns con tutti gli elementi a 0
     rectMatrix(options) {
         var matrix = new Array(options.numRows) //creo un array lungo numRows
-        for(var r = 0; r < options.numRows; r++) { //creo tanti array quanto vale numRows e tutti lunghi numColumns
+        for(let r = 0; r < options.numRows; r++) { //creo tanti array quanto vale numRows e tutti lunghi numColumns
             var row = new Array(options.numColumns)
             matrix[r] = row //creo la matrice con row come vettore riga, per ogni irga
-            for(var c = 0; c < options.numColumns; c++) {
+            for(let c = 0; c < options.numColumns; c++) {
                 row[c] = 0
             }
         }
@@ -183,8 +183,8 @@ class Regression {
 
     //percorro la matrice product e sommo ai valori che già ha
     addRowAndColumn(product,options){ //option contiene i valori da aggiungere etichettati nella funzione chiamante
-        for(var c = 0; c < options.lhsColumn.length; c++){
-            for(var r = 0; r < options.rhsRow.length; r++){
+        for(let c = 0; c < options.lhsColumn.length; c++){
+            for(let r = 0; r < options.rhsRow.length; r++){
                 product[c][r] += options.lhsColumn[c] * options.rhsRow[r]
             }
         }
