5d8a604aefad0bbff0baec3867e8596766a3b0ca
massimo <massimotoffoletto98@gmail.com>
2020-04-05 11:31:18 +0200

    Delete commenti read Test

--- src/tests/model/Read.test.js
+++ src/tests/model/Read.test.js
@@ -56,34 +56,3 @@ describe("Tests for class Read class", () => {
         expect(() => callbackFs(myError, null)).toThrowError(myError);
     });
 });
-
-
-   /* test('prints poem to console', done => {
-        //const logSpy = jest.spyOn(console, 'log');
-        let readFileCallback;
-        // @ts-ignore
-        jest.spyOn(fs, 'readFile').mockImplementation((path, options, callback) => {
-            readFileCallback = callback;
-        });
-
-        viewText();
-        readFileCallback(null, mockPoem);
-        expect(logSpy).toBeCalledWith(mockPoem);
-        expect(fs.readFile).toBeCalledWith('poem.txt', 'utf8', readFileCallback);
-        done();
-    });
-
-    test('should throw error when read file failed', done => {
-        let readFileCallback;
-        // @ts-ignore
-        jest.spyOn(fs, 'readFile').mockImplementation((path, options, callback) => {
-            readFileCallback = callback;
-        });
-
-        viewText();
-        const mError = new Error('read file failed');
-        expect(() => readFileCallback(mError, null)).toThrowError(mError);
-        expect(fs.readFile).toBeCalledWith('poem.txt', 'utf8', readFileCallback);
-        done();
-    });
-});*/
